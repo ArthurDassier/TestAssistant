b709e770e8e3e8069895cdf70e6c8fb322ea9640
Arthur Dassier <arthur.dassier@epitech.eu>
2019-06-08 13:38:15 +0200

    ex03 priest

--- test_assistant.php
+++ test_assistant.php
@@ -6,10 +6,10 @@ abstract class AHero
     protected $name;
     protected $hp;
 
-    public function __construct($string, $int=100)
+    public function __construct($string, int $int=100)
     {
-      $this->name = $string;
-      $this->hp = $int;
+        $this->name = $string;
+        $this->hp = $int;
     }
 
     public function __toString()
@@ -84,7 +84,7 @@ class Drood implements ISupport
         }
     }
 
-    public function setHeal($hero)
+    public function setHeal(int $value)
     {
         if (is_numeric($value) and !($value < 0))
             $this->_heal = $value;
@@ -93,15 +93,15 @@ class Drood implements ISupport
 
     public function getHeal()
     {
-        return $this->_heal;
+        return intval($this->_heal);
     }
 }
 
 interface IDPS
 {
     public function hitHero($hero);
-    public function setHeal($value);
-    public function getHeal();
+    public function setDMG($value);
+    public function getDMG();
 }
 
 class Rogue implements IDPS
@@ -123,7 +123,7 @@ class Rogue implements IDPS
         }
     }
 
-    public function setDMG($hero)
+    public function setDMG(int $value)
     {
         if (is_numeric($value) and !($value < 0))
             $this->_dmg = $value;
@@ -132,7 +132,109 @@ class Rogue implements IDPS
 
     public function getDMG()
     {
-        return $this->_dmg;
+        return intval($this->_dmg);
     }
 }
+
+// ------------- EX 03 --------------
+
+class Warrior extends AHero implements IDPS
+{
+    private $_dmg;
+    const MAX_HP = 200;
+    const BASIC_DMG = 10;
+
+    function __construct($string)
+    {
+        parent::__construct($string, self::MAX_HP);
+        $this->_dmg = self::BASIC_DMG;
+    }
+
+    public function hitHero($hero)
+    {
+        if ($hero->getHp() === 0)
+            return false;
+        else if ($this->_dmg === 0)
+            return false;
+        else if (($hero->getHp() - $_dmg) < 0) {
+            $hero->setHp(0);
+            return true;
+        } else {
+            $hero->setHp($hero->getHp() - $_dmg);
+            return true;
+        }
+    }
+
+    public function setDMG(int $hero)
+    {
+        if (is_numeric($value) and !($value < self::BASIC_DMG))
+            $this->_dmg = $value;
+        return $this;
+    }
+
+    public function getDMG()
+    {
+        return intval($this->_dmg);
+    }
+
+    public function __toString()
+    {
+        return "I'm a warrior hero and my name is ".$this->name." and I've got ".$this->hp." hp";
+    }
+
+    public function whoAmI()
+    {
+        return "I’m a warrior and I love to fight!\n";
+    }
+}
+
+class Priest extends AHero implements ISupport
+{
+    private $_heal;
+    const MAX_HP = 150;
+    const BASIC_HEAL = 12;
+
+    function __construct($string)
+    {
+        parent::__construct($string, self::MAX_HP);
+        $this->_heal = self::BASIC_HEAL;
+    }
+
+    public function healHero($hero)
+    {
+        if ($hero->getHp() === $hero::MAX_HP)
+            return false;
+        else if ($this->_heal === 0)
+            return false;
+        else if (($hero->getHp() + $_heal) > 100) {
+            $hero->setHp($hero->getHp() + (100 - $hero->getHp()));
+            return true;
+        } else {
+            $hero->setHp($hero->getHp() + $_heal);
+            return true;
+        }
+    }
+
+    public function setHeal(int $value)
+    {
+        if (is_numeric($value) and !($value < self::BASIC_HEAL))
+            $this->_heal = $value;
+        return $this;
+    }
+
+    public function getHeal()
+    {
+        return intval($this->_heal);
+    }
+
+    public function __toString()
+    {
+    }
+
+    public function whoAmI()
+    {
+        return "I'm a priest and I want to heal everyone!\n";
+    }
+}
+
 ?>
